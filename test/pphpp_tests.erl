a6915e491d6affba264b72717c7e44e225d1cbdc
CarlosDotDanger <carlos.danger@gmail.com>
2012-09-25 23:29:16 -0400

    removed outmoded tests

--- test/pphpp_tests.erl
+++ test/pphpp_tests.erl
@@ -2,8 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -include ("test_defs.hrl").
 
--import(pphpp,[	get_service_args/2,
-				get_service_args/4,
+-import(pphpp,[	
 				config_to_args/1]).
 
 
@@ -11,21 +10,19 @@
 args_test_()->
 	[
 		{"config_to_args", 
-			assert_config_args(config_to_args(?TEST_CONFIG))},
-       	{"name_and_script", 
-       	assert_args(get_service_args(?TEST_EXEC,?TEST_SCRIPT))},
-        {"name_script_max_and_timeout",
-        	assert_args(get_service_args(?TEST_EXEC,?TEST_SCRIPT,?TEST_MAX,?TEST_TIMEOUT))}
-   ].
+			assert_config_args(config_to_args(?TEST_CONFIG))}
+  ].
 
 %HELPERZ
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-assert_args(Term)->
-    fun() -> 
-        ?assertEqual(?SHORT_ARGS, Term)
-    end.
+
 
 assert_config_args(Term)->
+    fun() -> 
+        ?assertEqual(?LONG_ARGS, Term)
+    end.
+
+assert_service_spec(Term)->
     fun() -> 
         ?assertEqual(?LONG_ARGS, Term)
     end.
